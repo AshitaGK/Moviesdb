8c6d2af1e273e9132e5f6465a903f106b680765c
Ashita <ashita.gk696@gmail.com>
2020-02-05 19:29:14 +0530

    Updates

---
+++ precache-manifest.ac4917904ab4893035738b585c71188d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb6d1947d04123b478f55dd208d5be3",
+    "url": "/Moviesdb/index.html"
+  },
+  {
+    "revision": "34aebba1d72cadcec47b",
+    "url": "/Moviesdb/static/css/main.211b2ec3.chunk.css"
+  },
+  {
+    "revision": "5e5035c02c23cfbfee5d",
+    "url": "/Moviesdb/static/js/2.626bdba6.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/Moviesdb/static/js/2.626bdba6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34aebba1d72cadcec47b",
+    "url": "/Moviesdb/static/js/main.1ce0ccc9.chunk.js"
+  },
+  {
+    "revision": "1dcbb30cee5d3606382f",
+    "url": "/Moviesdb/static/js/runtime-main.a7509a48.js"
+  }
+]);
